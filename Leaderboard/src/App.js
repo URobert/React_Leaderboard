2087bb253f9bc7d57b50b5cc1017dcb13066bc4e
Robert U <robert.uivarosi@e-spres-oh.com>
2017-01-20 15:46:13 +0200

    WIP

--- Leaderboard/src/App.js
+++ Leaderboard/src/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className='topBar'>
+          <h3>FreeCodeCamp</h3>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br /><br />
+        <div className='title'>{this.props.name}</div>
+        <BoardContentComponent />
+        <TopCurrentUserList />
       </div>
-    );
+      );
   }
 }
+
+// // Main component - Application entry level
+// var LeaderBoardComponent = React.createClass ({
+//   render: function () {
+//     return (
+//       <div>
+//         <div className='topBar'>
+//           <h3>FreeCodeCamp</h3>
+//         </div>
+//         <br /><br />
+//         <div className='title'>{this.props.name}</div>
+//         <BoardContentComponent />
+//         <TopCurrentUserList />
+//       </div>
+//       );
+//   }
+// });
+
+//Board content component
+var BoardContentComponent = React.createClass ({
+  render: function (){
+    return (
+      <h2>Content goes here </h2>
+    );
+}
+});
+
+var TopCurrentUserList = React.createClass ({
+
+  componentDidMount () {
+    this.TopUserList();
+  },
+
+  TopUserList() {
+    return $.getJSON('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then((data) => {
+      this.setState({ user: data });
+    });
+  },
+
+  render: function () {
+    console.log(this.state);
+    return <div>{this.state}</div>
+    //
+    //   var users = this.state.user.map ((item, i) => {
+    //   return <div>
+    //     <h1>{item.state}</h1>
+    //     </div>
+    // });
+  }
+});
 
 export default App;
